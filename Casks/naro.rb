eb3a46ce3a02d3e37ecc6a1943f3e562ecf80d25
Sergei Sakharovskii <plexideas@gmail.com>
2026-09-22 22:06:05 +0200

    naro: update to 0.5.3

--- Casks/naro.rb
+++ Casks/naro.rb
@@ -1,8 +1,8 @@
 cask "naro" do
-  version "0.5.2"
-  sha256 "79c0bc7257d8aa9f70db1a7d971177d48f001edada5a314987dbc8d4f9ea3989"
+  version "0.5.3"
+  sha256 "39bdd1a0d1fe1fcbbd0dedabbf8d0decba26016d572962a48603eb67626b8b44"
 
-  url "https://github.com/plexideas/naro/releases/download/v#{version}/Naro-#{version}-15-arm64.zip"
+  url "https://github.com/plexideas/naro/releases/download/v#{version}/Naro-#{version}-16-arm64.zip"
   name "Naro"
   desc "Open Office files in Google editors and sync changes locally"
   homepage "https://github.com/plexideas/naro"
